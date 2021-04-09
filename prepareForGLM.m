229a2134d3e06b237f2341d6048a9d1b24a76bd3
Andrew Erskine <erskine94@gmail.com>
2021-04-09 14:36:10 -0700

    Initial GLM

---
+++ prepareForGLM.m
@@ -0,0 +1,73 @@
+clear all; close all; clc;
+
+data = load('AH1024_datastruct.mat');
+data = data.summary;
+fs = 15.44;
+
+% find highest performance trial
+correctRate = [data.CorrectRate];
+[m, maxIdx] = max(correctRate);
+session = maxIdx;
+
+% for that session get useful variables
+dff = dffFromTrace(data(session).c2FOVrigid);
+trialStart = data(session).trialStart;
+trialEnd = data(session).trialEnd;
+nTrials = length(trialStart);
+trialMatrix = data(session).trialMatrix;
+lickTimes = data(session).licks;
+waterTime = data(session).waterTime;
+
+figure; hold on;
+plot(dff, 'k');
+scatter(trialStart, ones(1,length(trialStart)), 'r.');
+
+% split into trials
+trialAligned = splitToTrials(dff', trialStart, trialEnd);
+
+% parse trialMatrix (hit, miss, fa, cr)
+trialOutcome = parseTrialMatrix(trialMatrix);
+
+figure;
+imagesc(trialAligned(30:end, 1:100))
+
+% creat GLM Matrix
+nFrames = 100;
+startTrial = 30;
+nVars = 2;
+
+% glm input - nobs x nvars (trials x vars)
+% glm output - nobs
+
+trialOutcome = trialOutcome(startTrial:end)';
+nLicks = cellfun(@length, lickTimes); nLicks = nLicks(startTrial:end);
+X = [trialOutcome nLicks];
+
+meanResponse = nanmean(trialAligned(startTrial:end, 1:nFrames), 2);
+y = meanResponse;
+
+fit = glmnet(X, y);
+
+% % cut out initial trials
+% trialAligned = trialAligned(30:end, :);
+% trialOutcome = trialOutcome(30:end);
+% lickTimes = lickTimes(30:end);
+% 
+% % show licks + response
+% figure; hold on;
+% imagesc(trialAligned);
+% 
+% for i = 1:size(lickTimes, 1)
+%     lickFrames = lickTimes{i}*fs;
+%     scatter(lickFrames, repmat(i, 1, length(lickFrames)), 'b.');
+% end
+% 
+% % show responses for each trial type;
+% figure;
+% subplot(2,2,1); imagesc(trialAligned(trialOutcome==1, :)); title('HIT');
+% subplot(2,2,2); imagesc(trialAligned(trialOutcome==2, :)); title('MISS');
+% subplot(2,2,3); imagesc(trialAligned(trialOutcome==4, :)); title('CR');
+% subplot(2,2,4); imagesc(trialAligned(trialOutcome==3, :)); title('FA');
+
+
+
--- sandbox.m
+++ sandbox.m
@@ -15,6 +15,8 @@ trialStart = data(session).trialStart;
 trialEnd = data(session).trialEnd;
 nTrials = length(trialStart);
 trialMatrix = data(session).trialMatrix;
+lickTimes = data(session).licks;
+waterTime = data(session).waterTime;
 
 figure; hold on;
 plot(dff, 'k');
@@ -23,15 +25,22 @@ scatter(trialStart, ones(1,length(trialStart)), 'r.');
 % split into trials
 trialAligned = splitToTrials(dff', trialStart, trialEnd);
 
-figure;
-imagesc(trialAligned(30:end, :));
-
 % parse trialMatrix (hit, miss, fa, cr)
 trialOutcome = parseTrialMatrix(trialMatrix);
 
 % cut out initial trials
 trialAligned = trialAligned(30:end, :);
 trialOutcome = trialOutcome(30:end);
+lickTimes = lickTimes(30:end);
+
+% show licks + response
+figure; hold on;
+imagesc(trialAligned);
+
+for i = 1:size(lickTimes, 1)
+    lickFrames = lickTimes{i}*fs;
+    scatter(lickFrames, repmat(i, 1, length(lickFrames)), 'b.');
+end
 
 % show responses for each trial type;
 figure;
