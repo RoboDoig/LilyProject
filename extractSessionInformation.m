7287e04fed002f6fb9aa97b09352f1a4714dfa18
Andrew Erskine <erskine94@gmail.com>
2021-05-12 19:28:17 -0700

    cont/.

--- extractSessionInformation.m
+++ extractSessionInformation.m
@@ -15,24 +15,32 @@ function [sessionStruct] = extractSessionInformation(data, sessionIdx, fs, trial
     alignInfoX = data(sessionIdx).alignInfo(:, 1);
     alignInfoY = data(sessionIdx).alignInfo(:, 2);
     lickTimesRelative = data(sessionIdx).licks(trialSkip:end, :);
-    waterTime = data(sessionIdx).waterTime(trialSkip: end);
+    waterTimesRelative = data(sessionIdx).waterTime(trialSkip: end);
     skipStartFrame = floor(trialStart(1)*fs);
     
     % get absolute times
     lickTimes = [];
+    waterTimes = [];
     poleOnset = [];
     poleDown = [];
     for i = 1:length(trialStart)
         lickTimes = [lickTimes, lickTimesRelative{i}'+trialStart(i)];
+        waterTimes = [waterTimes, waterTimesRelative{i}'+trialStart(i)];
         poleOnset = [poleOnset, poleOnsetRelative{i}+trialStart(i)];
         poleDown = [poleDown, poleDownRelative{i}+trialStart(i)];
     end
 
     % vectorize times
     lickTimesVec = zeros(1, length(dff));
+    waterTimesVec = zeros(1, length(dff));
     for i = 1:length(lickTimes)
         vecPoint = find(tAxis>=lickTimes(i), 1);
         lickTimesVec(vecPoint) = 1;
+    end
+    
+    for i = 1:length(waterTimes)
+        vecPoint = find(tAxis>=waterTimes(i), 1);
+        waterTimesVec(vecPoint) = 1;
     end
     
     poleOnsetVec = zeros(1, length(dff));
@@ -52,21 +60,22 @@ function [sessionStruct] = extractSessionInformation(data, sessionIdx, fs, trial
     sessionStruct.trialEnd = trialEnd;
     sessionStruct.nTrials = nTrials;
     sessionStruct.trialMatrix = trialMatrix;
-    sessionStruct.alignInfoX = alignInfoX;
-    sessionStruct.alignInfoY = alignInfoY;
+    sessionStruct.alignInfoX = alignInfoX';
+    sessionStruct.alignInfoY = alignInfoY';
     
     sessionStruct.lickTimesRelative = lickTimesRelative;
     sessionStruct.poleOnsetRelative = poleOnsetRelative;
     sessionStruct.poleDownRelative = poleDownRelative;
     
-    sessionStruct.waterTime = waterTime;
     sessionStruct.skipStartFrame = skipStartFrame;
     
     sessionStruct.lickTimes = lickTimes;
+    sessionStruct.waterTimes = waterTimes;
     sessionStruct.poleOnset = poleOnset;
     sessionStruct.poleDown = poleDown;
     
     sessionStruct.lickTimesVec = lickTimesVec;
+    sessionStruct.waterTimesVec = waterTimesVec;
     sessionStruct.poleOnsetVec = poleOnsetVec;
     sessionStruct.poleDownVec = poleDownVec;
     
@@ -78,6 +87,7 @@ function [sessionStruct] = extractSessionInformation(data, sessionIdx, fs, trial
         plot(tAxis, dff, 'k');
         plot(tAxis, lickTimesVec, 'b');
         plot(tAxis, poleOnsetVec, 'g');
+        plot(tAxis, waterTimesVec-1, 'y');
         plot([tAxis(skipStartFrame) tAxis(skipStartFrame)], [0, 1], 'g--')
         scatter(trialStart, ones(1,length(trialStart)), 'r.');
         scatter(lickTimes, ones(1,length(lickTimes)), 'b.');
---
+++ mouseGLMAnalysis.m
@@ -0,0 +1,55 @@
+function [allSessions] = mouseGLMAnalysis(data, fs, trialSkip, inputNames, windowSizes)
+
+    % fit models for each session
+    allSessions = cell(1, nSessions);
+    for sessionIdx = 1:nSessions
+        
+        fitData = struct();
+
+        % extract session variables
+        sessionStruct = extractSessionInformation(data.summary, sessionIdx, fs, trialSkip, 0);
+
+        % GLM
+        inputVectors = [];
+        for i = 1:length(inputNames)
+           inputVectors = [inputVectors; sessionStruct.(inputNames{i})]; 
+        end
+
+        responseVector = sessionStruct.dff; % response vector;
+        startFrame = sessionStruct.skipStartFrame;
+        endFrame = length(responseVector);
+
+        [fit, fullDesignMatrix, y, yHat] = buildGLM(inputVectors, windowSizes, responseVector, startFrame, endFrame, 0);
+
+        trueY = y(startFrame:endFrame);
+        yHat = yHat(:, end);
+        lickVec = sessionStruct.lickTimesVec(startFrame:endFrame);
+        t = sessionStruct.tAxis(startFrame:endFrame);
+
+        % extract response functions
+        cT = [0 cumsum(windowSizes)'];
+        responseFunctions = {};
+        for i = 1:size(inputVectors, 1)
+           rf = flipud(fit.beta((cT(i)+1):cT(i+1), end));
+           responseFunctions{i} = rf;
+        end
+
+        % save to session struct
+        fitData.sessionStruct = sessionStruct;
+        fitData.windowSizes = windowSizes;
+        fitData.responseVector = sessionStruct.dff;
+        fitData.startFrame = startFrame;
+        fitData.endFrame = endFrame;
+        fitData.fit = fit;
+        fitData.X = fullDesignMatrix;
+        fitData.trueY = trueY;
+        fitData.yHat = yHat;
+        fitData.lickVec = lickVec;
+        fitData.t = t;
+        fitData.responseFunctions = responseFunctions;
+        allSessions{sessionIdx} = fitData;
+        
+    end
+
+end
+
